bb5e0d44eca9ad3dfffb98e8b0bc2d0aa111e819
LawnMeower <s_schmidt1993@outlook.de>
2023-09-27 02:50:07 +0200

    had to redo stuff this way because git did the detached head thing again

--- MemCompareResult.cpp
+++ MemCompareResult.cpp
@@ -2,6 +2,8 @@
 #include"MemCompare.h"
 #include<filesystem>
 
+std::wstring MemoryCompare::MemCompareResults::_path = L"";
+
 MemoryCompare::MemCompareResults::MemCompareResults(std::wstring path, uint16_t iteration, uint8_t addressWidth, uint16_t valueWidth, uint16_t rangeCount, bool cached, bool zip)
 {
 	_path = path;
@@ -109,6 +111,24 @@ bool MemoryCompare::MemCompareResults::SaveResults(uint32_t rangeIndex, bool zip
 			return false;
 
 	return true;
+}
+
+void MemoryCompare::MemCompareResults::ClearResultsDir(const int iterationIndex)
+{
+	if (iterationIndex > -1)
+		for (const auto& entry : std::filesystem::directory_iterator(_path + std::to_wstring(iterationIndex)))
+		{
+			std::wcout << entry.path() << std::endl;
+			std::filesystem::remove_all(entry.path());
+		}
+	else
+		for (const auto& entry : std::filesystem::directory_iterator(_path))
+			std::filesystem::remove_all(entry.path());
+}
+
+void MemoryCompare::MemCompareResults::SetResultsDir(const std::wstring& dir)
+{
+	_path = dir;
 }
 
 bool MemoryCompare::MemCompareResults::LoadResults(bool zipped)
